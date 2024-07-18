151039d1402778c96addd29a4fde7960aa376a70
Adam Nemeth <nemethadam88@gmail.com>
2024-07-18 20:40:52 +0200

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <div>
       <h1>InterviewXpert</h1>
-      <p>This is a tool to help you prepare for your next interview.</p>
+      <p>
+        This is a tool to help you prepare for your next interview as an
+        interviewer.
+      </p>
       <Button />
     </div>
   );
